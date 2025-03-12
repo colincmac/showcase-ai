61d51bb99237239e448fa86f0636d4d98c116c6c
Colin McCullough <cmccullough@microsoft.com>
2025-03-12 16:20:09 -0400

    Updating

--- VoiceRAG/Showcase.AudioOrchestration/AudioTranscriptionStore.cs
+++ VoiceRAG/Showcase.AudioOrchestration/AudioTranscriptionStore.cs
@@ -6,14 +6,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Showcase.AudioOrchestration;
 
 public interface IConversationStore
 {
-    Task AppendConversationHistoryAsync(string callId, ConversationItem conversationItem);
-    Task<IList<TranscriptItem>> GetConversationHistoryAsync(string callId);
+    Task AppendConversationHistoryAsync(string conversationId, ConversationUpdate conversationItem);
+    Task<IEnumerable<ConversationUpdate>> GetConversationHistoryAsync(string conversationId);
 }
 
 public record TranscriptItem(string Role, string Content);
@@ -25,27 +26,17 @@ public class RedisConversationStore : IConversationStore
     {
         _redis = redisConnection.GetDatabase();
     }
-    public async Task AppendConversationHistoryAsync(string callId, ConversationItem conversationItem)
+    public async Task AppendConversationHistoryAsync(string conversationId, ConversationUpdate conversationItem)
     {
-
-        await _redis.ListRightPushAsync($"transcripts:{callId}", $"user:{text}");
+        var item = JsonSerializer.Serialize(conversationItem);
+        await _redis.ListRightPushAsync(conversationId, item);
     }
 
-    public async Task<IList<TranscriptItem>> GetConversationHistoryAsync(string callId)
+    public async Task<IEnumerable<ConversationUpdate>> GetConversationHistoryAsync(string conversationId)
     {
-        var entries = await _redis.ListRangeAsync($"transcripts:{callId}", 0, -1);
-        var history = new List<TranscriptItem>();
-        foreach (var entry in entries)
-        {
-            var val = entry.ToString();
-            int sep = val.IndexOf(':');
-            if (sep > 0)
-            {
-                string role = val.Substring(0, sep);
-                string content = val.Substring(sep + 1);
-                history.Add(new TranscriptItem(role, content));
-            }
-        }
-        return history;
+        var entries = await _redis.ListRangeAsync(conversationId, -1);
+        return entries
+            .Select(entry => JsonSerializer.Deserialize<ConversationUpdate>(entry.ToString()))
+            .OfType<ConversationUpdate>();
     }
 }
